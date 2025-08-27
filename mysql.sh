b4e39d635215c302e3d79eed7cf8074adffb0505
aravind kumar <aravindkumarreddy66@gmail.com>
2025-08-28 00:13:26 +0530

    functions

---
+++ mysql.sh
@@ -0,0 +1,41 @@
+ #!/bin/bash
+ LOGS_FOLDER="/var/log/expense"
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+ mkdir -p $LOGS_FOLDER
+
+ USERID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[0m"
+ Y="\e[33m"
+
+  if [ $USERID -ne 0 ]
+ then
+    echo -e "$R please run this script with root previlizes $N" | tee -a $LOG_FILE
+    exit 1
+ fi
+ 
+ validate() {
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi    
+ }
+
+
+ echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+ dnf install mysql-server -y &>>$LOG_FILE
+ VALIDATE $? "Installing MySQL Server"
+
+ systemctl enable mysqld &>>$LOG_FILE
+ VALIDATE $? "Enabled MySQL Server"
+
+ systemctl start mysqld &>>$LOG_FILE
+ VALIDATE $? "Started MySQL server" 
